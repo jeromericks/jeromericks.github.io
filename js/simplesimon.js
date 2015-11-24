21cd95edfdd3085e97c739ab1163efc58e94fc9c
jeromericks <jeromericks@gmail.com>
2015-11-23 19:19:30 -0600

    Edited JS file for Simple Simon

--- js/simplesimon.js
+++ js/simplesimon.js
@@ -1,101 +1,98 @@
+var colorArr = ['green', 'red', 'yellow', 'blue'];
+var playerTurn = false;
+var simonArr = [];
+var playerArr = [];
+var randomColor = 0;
+var playerGuess = '';
+var idx = 0;
+var count = 0;
+var gamePower = true;
+
+var startSequence = ['green', 'red', 'blue', 'green', 'yellow', 'red', 'blue', 'green', 'yellow', 'red', 'green', 'red', 'green', 'yellow', 'blue', 'red', 'green', 'blue', 'red', 'yellow', 'green', 'red', 'blue', 'yellow'];
+
+// Making variables for all audio that is needed.
+var greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+var redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+var wrongSound = new Audio('https://www.freesound.org/data/previews/142/142608_1840739-lq.mp3');
+var soundsArr = [greenSound, redSound, yellowSound, blueSound];
+
+
+function startItUp(sequence) {
+	// First setTimeout is delay between pressing start and starting intro sequence.
+	setTimeout(function() {
+		for (var i = 0; i < sequence.length; i++) {
+			(function(order) {
+				// Second setTimeout is for delay between computer moves.
+				setTimeout(function() {
+					$('.' + sequence[order]).addClass(sequence[order] + '-glow');
+					// Third setTimeout is for delay between removing glow effect.
+					setTimeout(function() {
+						$('.' + sequence[order]).removeClass(sequence[order] + '-glow')
+					}, 200);
+				}, order * 250);
+			})(i);
+		}
+	}, 100);
+	// Fourth setTimeout is for delay before starting game waiting for initial sequence to end.
+	setTimeout(function() {
+		nextColor();
+		lightAndSound(simonArr);
+	}, 7100);
+}
+
+// Getting a color and pushing it to an array of computer moves.
+function nextColor() {
+	randomColor = colorArr[Math.floor(Math.random() * 4)];
+	simonArr.push(randomColor);
+	return simonArr;
+}
+
+// Adding a glow effect and sound to computer moves.
+function lightAndSound(simon) {
+	// First setTimeout is delay between player move ending and computer move starting.
+	setTimeout(function() {
+		for (var i = 0; i < simon.length; i++) {
+			(function(order) {
+				// Second setTimeout is for delay between computer moves.
+				setTimeout(function() {
+					soundIdx = colorArr.indexOf(simon[order]);
+					soundsArr[soundIdx].play();
+					$('.' + simon[order]).addClass(simon[order] + '-glow');
+					// Third setTimeout is for delay between removing glow effect.
+					setTimeout(function() {
+						$('.' + simon[order]).removeClass(simon[order] + '-glow')
+					}, speed());
+				}, order * (speed() + 50));
+			})(i);
+		}
+	}, 850);
+	playerTurn = true;
+}
+
+// Determining the time between computer moves based on skill level.
+function speed() {
+	if (count < 5) {
+		return 700;
+	} else if (count < 13) {
+		return 650;
+	} else {
+		return 600;
+	}
+}
+
+// Adding to the current score.
+function score() {
+	count++;
+	if (count < 10) {
+		$('.count').html('0' + count);
+	} else {
+		$('.count').html(count);
+	}
+}
+
 $(document).ready(function() {
-	"use strict";
-
-	var colorArr = ['green', 'red', 'yellow', 'blue'];
-	var playerTurn = false;
-	var simonArr = [];
-	var playerArr = [];
-	var randomColor = 0;
-	var playerGuess = '';
-	var idx = 0;
-	var count = 0;
-	var gamePower = true;
-
-	var startSequence = ['green', 'red', 'blue', 'green', 'yellow', 'red', 'blue', 'green', 'yellow', 'red', 'green', 'red', 'green', 'yellow', 'blue', 'red', 'green', 'blue', 'red', 'yellow', 'green', 'red', 'blue', 'yellow'];
-
-	// Making variables for all audio that is needed.
-	var greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
-	var redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
-	var yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
-	var blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
-	var wrongSound = new Audio('https://www.freesound.org/data/previews/142/142608_1840739-lq.mp3');
-	var soundsArr = [greenSound, redSound, yellowSound, blueSound];
-	var soundIdx;
-
-
-	function startItUp(sequence) {
-		// First setTimeout is delay between pressing start and starting intro sequence.
-		setTimeout(function() {
-			for (var i = 0; i < sequence.length; i++) {
-				(function(order) {
-					// Second setTimeout is for delay between computer moves.
-					setTimeout(function() {
-						$('.' + sequence[order]).addClass(sequence[order] + '-glow');
-						// Third setTimeout is for delay between removing glow effect.
-						setTimeout(function() {
-							$('.' + sequence[order]).removeClass(sequence[order] + '-glow')
-						}, 200);
-					}, order * 250);
-				})(i);
-			}
-		}, 100);
-		// Fourth setTimeout is for delay before starting game waiting for initial sequence to end.
-		setTimeout(function() {
-			nextColor();
-			lightAndSound(simonArr);
-		}, 7100);
-	}
-
-	// Getting a color and pushing it to an array of computer moves.
-	function nextColor() {
-		randomColor = colorArr[Math.floor(Math.random() * 4)];
-		simonArr.push(randomColor);
-		return simonArr;
-	}
-
-	// Adding a glow effect and sound to computer moves.
-	function lightAndSound(simon) {
-		// First setTimeout is delay between player move ending and computer move starting.
-		setTimeout(function() {
-			for (var i = 0; i < simon.length; i++) {
-				(function(order) {
-					// Second setTimeout is for delay between computer moves.
-					setTimeout(function() {
-						soundIdx = colorArr.indexOf(simon[order]);
-						soundsArr[soundIdx].play();
-						$('.' + simon[order]).addClass(simon[order] + '-glow');
-						// Third setTimeout is for delay between removing glow effect.
-						setTimeout(function() {
-							$('.' + simon[order]).removeClass(simon[order] + '-glow')
-						}, speed());
-					}, order * (speed() + 50));
-				})(i);
-			}
-		}, 850);
-		playerTurn = true;
-	}
-
-	// Determining the time between computer moves based on skill level.
-	function speed() {
-		if (count < 5) {
-			return 700;
-		} else if (count < 13) {
-			return 650;
-		} else {
-			return 600;
-		}
-	}
-
-	// Adding to the current score.
-	function score() {
-		count++;
-		if (count < 10) {
-			$('.count').html('0' + count);
-		} else {
-			$('.count').html(count);
-		}
-	}
-
 	$('.game-container').on('click', '.gameboard', function(e) {
 		e.stopPropagation();
 		if (playerTurn === true) {
